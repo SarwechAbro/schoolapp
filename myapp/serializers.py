a737ff08f6838a3cf8311bcea331e98440a7f635
SarwechAbro <sarwechabro@gmail.com>
2024-12-27 15:46:42 +0500

    serializers updated with related table

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,37 +1,41 @@
 from rest_framework import serializers
 from .models import Teacher, Student, Class, Course, Chapter, Lecture
 
-class StudentSerializer(serializers.ModelSerializer):
+class ClassSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Student
+        model = Class
         fields = '__all__'
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'        
-
-
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Teacher 
         fields = '__all__'        
 
-
-
-class ClassSerializer(serializers.ModelSerializer):
+class StudentSerializer(serializers.ModelSerializer):
+    teacher_id = TeacherSerializer(many=True, read_only=True)
+    
     class Meta:
-        model = Class
+        model = Student
         fields = '__all__'
 
+class CourseSerializer(serializers.ModelSerializer):
+    class_id = ClassSerializer(read_only=True)
+    teacher_id = TeacherSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Course
+        fields = '__all__'        
+
 class ChapterSerializer(serializers.ModelSerializer):
+    course_id = CourseSerializer(read_only=True)
+    
     class Meta:
         model = Chapter
         fields = '__all__'
 
 class LectureSerializer(serializers.ModelSerializer):    
+    chapter_id = ChapterSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Lecture 
-        fields = '__all__'        
+        fields = '__all__'
