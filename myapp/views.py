21d18123c5c12aba6a95523491df006bee165516
SarwechAbro <sarwechabro@gmail.com>
2025-01-01 19:25:50 +0500

    comment

--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics
 
 
 
-
+#just comment
 
 #Teacher APIs
 class TeacherAPI(generics.RetrieveAPIView, generics.CreateAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
